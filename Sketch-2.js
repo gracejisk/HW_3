301ca01804258cf737ee2d32a526ece614e5016d
gracejisk <park.grace@cca.edu>
2017-02-05 23:32:14 -0800

    Create Sketch-2.js

---
+++ Sketch-2.js
@@ -0,0 +1,18 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(255);
+	
+	var x=200;
+	var y=200;
+var w=0;
+var h=0;
+			while(w<400,h<400){
+				noFill();
+				ellipse (200,200,w,h);
+				w=w+10;
+				h=h+10;
+}
+}
